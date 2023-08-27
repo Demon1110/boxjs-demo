957bed271297278772cd707f9c3d4b95f6ed7552
wxy <852232096@qq.com>
2023-08-27 13:25:13 +0800

    qq

--- wxread/wx.js
+++ wxread/wx.js
@@ -1,6 +1,7 @@
 const chavy = init()
 // $.KEY_mobile = 'wxread_cary'
 
+const KEY_signkey = 'wxread_signkey_cary'
 const KEY_signheader = 'wxread_signheader_cary'
 const KEY_signbody = 'wxread_signbody_cary'
 const KEY_mobile = 'wxread_mobile_cary'
@@ -14,17 +15,21 @@ const KEY_mobile = 'wxread_mobile_cary'
 function showmsg() {
   return new Promise((resolve) => {
     // if (this.$request && this.$request.headers && this.$request.body) {
-    //console.log(this)
-    if (this.$request) {
+    if (this.$request && this.$request.headers) {
       chavy.log(KEY_signheader);
       chavy.log(KEY_signbody);
       chavy.log(KEY_mobile);
       const VAL_signheader = JSON.stringify($request.headers)
-      const VAL_signbody = this.$request.body
       if (VAL_signheader) {
         chavy.setdata(VAL_signheader, KEY_signheader)
-        chavy.msg("VAL_signheader", `获取Cookie: 成功`, VAL_signheader)
+        // chavy.msg("VAL_signheader", `获取Cookie: 成功`, VAL_signheader)
       }
+      if ($request.headers && $request.headers['skey']) {
+        const skey = $request.headers['skey']
+        chavy.setdata(KEY_signkey, skey)
+        chavy.msg("KEY_signkey", `获取skey: 成功`, skey)
+      }
+      const VAL_signbody = this.$request.body
       if (VAL_signbody) {
         chavy.setdata(VAL_signbody, KEY_signbody)
         chavy.msg("VAL_signbody", `获取Cookie: 成功`, VAL_signbody)
