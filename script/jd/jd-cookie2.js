9f0dbc8c54918df7a691173ea76a85c55f08504d
wxy <852232096@qq.com>
2024-10-18 15:11:38 +0800

    222

---
+++ script/jd/jd-cookie2.js
@@ -0,0 +1,72 @@
+const chavy = init()
+
+const KEY_signheader = 'wxread_signheader_cary'
+const KEY_mobile = 'wxread_mobile_cary'
+
+!(async () => {
+  await showmsg()
+})()
+  .catch((e) => KEY_mobile.log(e))
+  .finally(() => KEY_mobile.done())
+
+function showmsg() {
+  return new Promise((resolve) => {
+    if (this.$request && this.$request.headers) {
+      const VAL_signheader = JSON.stringify($request.headers)
+      if (VAL_signheader) {
+        chavy.setdata(KEY_signheader, VAL_signheader)
+        chavy.msg('VAL_signheader', `获取Header: 成功`, VAL_signheader)
+      }
+      const cookies = $request.headers['Cookie']
+      chavy.msg('cookieName', `获取Cookie: 成功`, JSON.stringify(cookies))
+    } else {
+      chavy.msg('cookieName', `获取Cookie: 失败`, ``)
+    }
+    resolve()
+  })
+}
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+chavy.done()
